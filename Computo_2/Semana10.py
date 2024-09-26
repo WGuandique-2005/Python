ca4e591f421547bcb92a88336fe627c6f43a9c3f
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-09-26 10:04:20 -0600

    commit

--- Computo_2/Semana10.py
+++ Computo_2/Semana10.py
@@ -5,5 +5,16 @@
 
 import pandas as pd
 
-df = pd.read_csv("empresa.producto.csv")
-print(df)
+
+#! Hacer por columnnas
+df = pd.read_csv("steam.csv")
+subsel = df[["Name","Price","Review_type"]]
+print(subsel.head(30))
+
+#! Para ordenar por una columna
+subsel = df[["Name","Price","Review_type"]].sort_values(by="Price",ascending=False)
+print(subsel.head(30))
+
+
+#! Para buscar en base a un valor 
+subsel = df[["Name","Price","Review_type"]][(df["Price"]<=50.0)&(df["Price"]!=0.0)]
