6738523f926eca47626ac638d13e388c93d593ba
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-11-05 10:33:48 -0600

    commit

--- Computo 3/semana15/app1/views.py
+++ Computo 3/semana15/app1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+def index(request):
+    return render(request, 'index.html')
 
+def base(request):
+    return render(request, 'base.html')
 # Create your views here.
